33ac15257a6305dc901c3996d034baf83cd7e938
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-04 22:17:14 +0530

    Create 2main.go

---
+++ 2main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num1 int16
+	var num2 float32
+	var firstnum string
+	var married bool
+
+	fmt.Println(num1)
+	fmt.Println(num2)
+	fmt.Println(firstnum)
+	fmt.Println(married)
+
+}
+
+
+o/p - >
+
+$ go run main.go
+0
+0
+
+false
