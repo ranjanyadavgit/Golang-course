866fd557328f783d9a069ea121a896f3aa20fdc6
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-04 22:23:05 +0530

    Create 3main.go

---
+++ 3main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		num1     = 12
+		num2     = 13
+		firstnum = "Ranjan"
+		married  = false
+	)
+
+	fmt.Println(num1)
+	fmt.Println(num2)
+	fmt.Println(firstnum)
+	fmt.Println(married)
+
+}
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /d/Study/Golang/src/variables
+$ go run main.go
+12
+13
+Ranjan
+false
