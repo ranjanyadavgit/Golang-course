d57a5532f120506700461afceb8b64e2807af315
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-06 19:03:10 +0530

    Create 6main.go

---
+++ 6main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var globalVariable int = 100
+
+func main() {
+
+	var localVariable int = 200
+
+	fmt.Printf("the value of the global variable is: %d\n", globalVariable)
+	fmt.Printf("the value of the local variable is: %d\n", localVariable)
+
+}
