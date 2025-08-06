f426d95948d67b166a9c82f74e3f857aea40090a
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-06 21:17:49 +0530

    Create 5main.go

---
+++ datatypes/5main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	str := "Go programming language"
+
+	fmt.Printf("the value of string str is: %s\n", str)
+	fmt.Printf("the type of string str is: %T\n", str)
+	fmt.Printf("the length of string str is: %d\n", len(str))
+
+}
+
+o/p->$ go run main.go
+the value of string str is: Go programming language
+the type of string str is: string
+the length of string str is: 23
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /d/Study/Golang/src/datatypes
+$ 
