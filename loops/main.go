8dbfae8f3f7af89ac1866945de45bdd6177f4151
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-09 22:51:26 +0530

    Create main.go

---
+++ loops/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 5; i++ {
+
+		fmt.Printf("i= %d\t Go programming language\n", i)
+	}
+
+}
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /d/Study/Golang/src/loops
+$ go run main.go
+i= 0     Go programming language
+i= 1     Go programming language
+i= 2     Go programming language
+i= 3     Go programming language
+i= 4     Go programming language
