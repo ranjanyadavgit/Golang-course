587af697354acc5d7d06a9a5c9d16e825a4ff406
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-09 20:47:01 +0530

    Update 3main.go

--- decisionmaking/3main.go
+++ decisionmaking/3main.go
@@ -19,3 +19,10 @@ func main() {
 	fmt.Printf("Value of A: %d\n", a)
 	fmt.Printf("Value of B: %d\n", b)
 }
+
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /d/Study/Golang/src/decisionmaking
+$ go run 3main.go
+Value of A and B are Equal
+Value of A: 300
+Value of B: 500
