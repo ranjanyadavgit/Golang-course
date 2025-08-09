71cf51d06709da2d9a2db52c3090ebb4f26d53f5
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-09 17:11:43 +0530

    Create comparison.go

---
+++ operators/comparison.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 50
+	b := 40
+
+	result1 := a != b
+
+	fmt.Println("A != B :", result1)
+}
+
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /d/Study/Golang/src/operators
+$ go run comparison.go 
+A != B : true
