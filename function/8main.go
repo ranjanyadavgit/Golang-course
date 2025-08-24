758d9f3848962e859aa123e6a3ecea90ce80738e
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-24 20:29:08 +0530

    Create 8main.go

---
+++ function/8main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	result := func() {
+
+		fmt.Println("Welcome to Go programming")
+
+	}
+	result()
+}
+
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /d/Study/Golang/src/functions
+$ go run 9main.go 
+Welcome to Go programming
