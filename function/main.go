067d71a7bc23c5fb1e18a165b562b128233b48f3
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-24 17:20:14 +0530

    Create main.go

---
+++ function/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+func simpleFunction(){
+
+  fmt.Println("hello world")
+  }
+
+func main(){
+  simpleFunction()
+  }
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /d/Study/Golang/src/functions
+$ go run main.go
+hello world
