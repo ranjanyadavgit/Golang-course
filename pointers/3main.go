fc2aa00a1e510c14eca41f5cd1f555a4194f778a
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-28 12:08:44 +0530

    Update 3main.go

--- pointers/3main.go
+++ pointers/3main.go
@@ -11,11 +11,14 @@ func main() {
 	fmt.Println("the address stored in a is :", &a)
 	fmt.Println("the value of p is:", p)
 	fmt.Println("the value of p before changing is:", *p)
-}
 
+	*p = 500
+	fmt.Println("value stored in a after changing:", a)
+	
 Lenovo@DESKTOP-M5DT73G MINGW64 /d/Study/Golang/src/pointers
 $ go run 3main.go
 the value of a is: 450
 the address stored in a is : 0xc00000a0e8
 the value of p is: 0xc00000a0e8
 the value of p before changing is: 450
+value stored in a after changing: 500
