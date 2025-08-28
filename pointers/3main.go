f8d80d9b353f697e636bb810b305c84943a022b8
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-28 10:48:12 +0530

    Create 3main.go

---
+++ pointers/3main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a = 450
+	var p = &a
+
+	fmt.Println("the value of a is:", a)
+	fmt.Println("the address stored in a is :", &a)
+	fmt.Println("the value of p is:", p)
+	fmt.Println("the value of p before changing is:", *p)
+}
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /d/Study/Golang/src/pointers
+$ go run 3main.go
+the value of a is: 450
+the address stored in a is : 0xc00000a0e8
+the value of p is: 0xc00000a0e8
+the value of p before changing is: 450
