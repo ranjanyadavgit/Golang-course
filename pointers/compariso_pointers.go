efc44371a9dd5882a83c3a00b5991dbdf8bfc5e3
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-05 00:06:18 +0530

    Update compariso_pointers.go

--- pointers/compariso_pointers.go
+++ pointers/compariso_pointers.go
@@ -26,4 +26,21 @@ func main() {
 	result4 := &p1 == &p3
 
 	fmt.Println("the result: p1 equals p3", result4)
+
+	fmt.Println("the result: p2 equals p3", result5)
+
+	result6 := &p1 == &p2
+
+	fmt.Println("the result: p1 equals p2", result6)
 }
+
+
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /d/Study/Golang/src/Golang_basics/pointers
+$ go run comparison_pointer.go 
+the result1: p1 equals p2 false
+the result: p1 equuls p3 true
+the result: p2 equals p3 false
+the result: p1 equals p3 false
+the result: p2 equals p3 false
+the result: p1 equals p2 false
