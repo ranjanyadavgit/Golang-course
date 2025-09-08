c52798c8080e0d0229e0c1085e4a031247abbcec
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-08 21:11:03 +0530

    Create 2main.go

---
+++ Strings/2main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	str1 := "Go Lang programming language"
+
+	fmt.Println(str1)
+
+	//str1[1]=`w`
+}
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /d/Study/Golang/src/Golang_basics/Strings
+$ go run 2main.go 
+Go Lang programming language
