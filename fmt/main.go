32fa29f1d60788c24cc575379d0b095a07b130e8
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-09 00:50:01 +0530

    Create main.go

---
+++ fmt/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var str1, str2, str3 = "a", "b", "c"
+
+	fmt.Print(str1, str2, str3)
+
+	var num1, num2, num3 = 1, 2, 3
+	fmt.Print(num1, num2, num3)
+
+}
+-------------
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /d/Study/Golang/src/Golang_basics/fmt
+$ go run main.go
+abc1 2 3
+Lenovo@DESKTOP-M5DT73G MINGW64 /
